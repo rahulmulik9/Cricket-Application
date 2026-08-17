55c563660d0fae8febf1c7b1da031d8b7b949fdd
Rahul Mulik <rahulmulik9@gmail.com>
2026-08-17 11:31:19 +0530

    sql sheet added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,105 @@
+-- ===== TEAMS =====
+-- Group A
+INSERT INTO teams (name, short_name, group_name) VALUES ('Mumbai Strikers', 'MUM', 'A');
+INSERT INTO teams (name, short_name, group_name) VALUES ('Chennai Lions', 'CHE', 'A');
+INSERT INTO teams (name, short_name, group_name) VALUES ('Delhi Warriors', 'DEL', 'A');
+INSERT INTO teams (name, short_name, group_name) VALUES ('Bangalore Titans', 'BAN', 'A');
+INSERT INTO teams (name, short_name, group_name) VALUES ('Kolkata Riders', 'KOL', 'A');
+
+-- Group B
+INSERT INTO teams (name, short_name, group_name) VALUES ('Punjab Kings', 'PUN', 'B');
+INSERT INTO teams (name, short_name, group_name) VALUES ('Rajasthan Royals', 'RAJ', 'B');
+INSERT INTO teams (name, short_name, group_name) VALUES ('Hyderabad Sultans', 'HYD', 'B');
+INSERT INTO teams (name, short_name, group_name) VALUES ('Gujarat Sharks', 'GUJ', 'B');
+INSERT INTO teams (name, short_name, group_name) VALUES ('Pune Panthers', 'PUNE', 'B');
+
+-- ===== PLAYERS =====
+-- Team 1: Mumbai Strikers
+INSERT INTO players (name, jersey_number, team_id) VALUES ('MUM Player 1', 1, 1);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('MUM Player 2', 2, 1);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('MUM Player 3', 3, 1);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('MUM Player 4', 4, 1);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('MUM Player 5', 5, 1);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('MUM Player 6', 6, 1);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('MUM Player 7', 7, 1);
+
+-- Team 2: Chennai Lions
+INSERT INTO players (name, jersey_number, team_id) VALUES ('CHE Player 1', 1, 2);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('CHE Player 2', 2, 2);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('CHE Player 3', 3, 2);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('CHE Player 4', 4, 2);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('CHE Player 5', 5, 2);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('CHE Player 6', 6, 2);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('CHE Player 7', 7, 2);
+
+-- Team 3: Delhi Warriors
+INSERT INTO players (name, jersey_number, team_id) VALUES ('DEL Player 1', 1, 3);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('DEL Player 2', 2, 3);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('DEL Player 3', 3, 3);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('DEL Player 4', 4, 3);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('DEL Player 5', 5, 3);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('DEL Player 6', 6, 3);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('DEL Player 7', 7, 3);
+
+-- Team 4: Bangalore Titans
+INSERT INTO players (name, jersey_number, team_id) VALUES ('BAN Player 1', 1, 4);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('BAN Player 2', 2, 4);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('BAN Player 3', 3, 4);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('BAN Player 4', 4, 4);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('BAN Player 5', 5, 4);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('BAN Player 6', 6, 4);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('BAN Player 7', 7, 4);
+
+-- Team 5: Kolkata Riders
+INSERT INTO players (name, jersey_number, team_id) VALUES ('KOL Player 1', 1, 5);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('KOL Player 2', 2, 5);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('KOL Player 3', 3, 5);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('KOL Player 4', 4, 5);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('KOL Player 5', 5, 5);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('KOL Player 6', 6, 5);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('KOL Player 7', 7, 5);
+
+-- Team 6: Punjab Kings
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUN Player 1', 1, 6);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUN Player 2', 2, 6);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUN Player 3', 3, 6);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUN Player 4', 4, 6);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUN Player 5', 5, 6);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUN Player 6', 6, 6);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUN Player 7', 7, 6);
+
+-- Team 7: Rajasthan Royals
+INSERT INTO players (name, jersey_number, team_id) VALUES ('RAJ Player 1', 1, 7);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('RAJ Player 2', 2, 7);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('RAJ Player 3', 3, 7);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('RAJ Player 4', 4, 7);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('RAJ Player 5', 5, 7);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('RAJ Player 6', 6, 7);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('RAJ Player 7', 7, 7);
+
+-- Team 8: Hyderabad Sultans
+INSERT INTO players (name, jersey_number, team_id) VALUES ('HYD Player 1', 1, 8);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('HYD Player 2', 2, 8);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('HYD Player 3', 3, 8);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('HYD Player 4', 4, 8);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('HYD Player 5', 5, 8);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('HYD Player 6', 6, 8);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('HYD Player 7', 7, 8);
+
+-- Team 9: Gujarat Sharks
+INSERT INTO players (name, jersey_number, team_id) VALUES ('GUJ Player 1', 1, 9);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('GUJ Player 2', 2, 9);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('GUJ Player 3', 3, 9);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('GUJ Player 4', 4, 9);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('GUJ Player 5', 5, 9);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('GUJ Player 6', 6, 9);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('GUJ Player 7', 7, 9);
+
+-- Team 10: Pune Panthers
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUNE Player 1', 1, 10);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUNE Player 2', 2, 10);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUNE Player 3', 3, 10);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUNE Player 4', 4, 10);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUNE Player 5', 5, 10);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUNE Player 6', 6, 10);
+INSERT INTO players (name, jersey_number, team_id) VALUES ('PUNE Player 7', 7, 10);
